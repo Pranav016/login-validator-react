76127b9f9db1867ea6133d768e54bef66ddac586
Pranav <pranavmendi@gmail.com>
2021-04-29 16:35:55 +0530

    Add Input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContext from '../../context/auth-context';
+import Input from '../Input/Input';
 
 const emailReducer = (state, action) => {
 	if (action.type === 'USER_INPUT') {
@@ -101,34 +102,22 @@ const Login = (props) => {
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
-				<div
-					className={`${classes.control} ${
-						emailState.isValid === false ? classes.invalid : ''
-					}`}>
-					<label htmlFor='email'>E-Mail</label>
-					<input
-						type='email'
-						id='email'
-						// value={enteredEmail}
-						value={emailState.value}
-						onChange={emailChangeHandler}
-						onBlur={validateEmailHandler}
-					/>
-				</div>
-				<div
-					className={`${classes.control} ${
-						passwordState.isValid === false ? classes.invalid : ''
-					}`}>
-					<label htmlFor='password'>Password</label>
-					<input
-						type='password'
-						id='password'
-						// value={enteredPassword}
-						value={passwordState.value}
-						onChange={passwordChangeHandler}
-						onBlur={validatePasswordHandler}
-					/>
-				</div>
+				<Input
+					id='email'
+					type='email'
+					label='E-Mail'
+					value={emailState.value}
+					isValid={emailIsValid}
+					onChange={emailChangeHandler}
+					onBlur={validateEmailHandler}></Input>
+				<Input
+					id='password'
+					type='password'
+					label='Password'
+					value={passwordState.value}
+					isValid={passwordIsValid}
+					onChange={passwordChangeHandler}
+					onBlur={validatePasswordHandler}></Input>
 				<div className={classes.actions}>
 					<Button type='submit' className={classes.btn} disabled={!formIsValid}>
 						Login
